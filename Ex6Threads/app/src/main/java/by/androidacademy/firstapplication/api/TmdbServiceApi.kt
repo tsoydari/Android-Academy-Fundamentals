aa482932eebabcaf8a1465556fefbff456fd1fdb
Tsoy Darya <tsoydari@gmail.com>
2020-11-24 18:59:11 +0300

    Create TMDB popular movies call

---
+++ Ex6Threads/app/src/main/java/by/androidacademy/firstapplication/api/TmdbServiceApi.kt
@@ -0,0 +1,23 @@
+package by.androidacademy.firstapplication.api
+
+import by.androidacademy.firstapplication.api.dto.MovieVideosDto
+import by.androidacademy.firstapplication.api.dto.PopularMoviesDto
+import by.androidacademy.firstapplication.utils.SERVER_API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TmdbServiceApi {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String = SERVER_API_KEY
+    ): PopularMoviesDto
+
+
+    @GET("movie/{movieId}/videos")
+    suspend fun getMovieVideos(
+        @Path("movieId") movieId: Int,
+        @Query("api_key") apiKey: String = SERVER_API_KEY
+    ): MovieVideosDto
+}
