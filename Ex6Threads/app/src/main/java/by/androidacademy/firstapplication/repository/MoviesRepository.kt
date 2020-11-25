69796fcff590583cd046eacf34e3f44858206ccb
Tsoy Darya <tsoydari@gmail.com>
2020-11-25 21:41:04 +0300

    Update database from Repository

--- Ex6Threads/app/src/main/java/by/androidacademy/firstapplication/repository/MoviesRepository.kt
+++ Ex6Threads/app/src/main/java/by/androidacademy/firstapplication/repository/MoviesRepository.kt
@@ -2,20 +2,48 @@ package by.androidacademy.firstapplication.repository
 
 import by.androidacademy.firstapplication.api.TmdbServiceApi
 import by.androidacademy.firstapplication.data.Movie
+import by.androidacademy.firstapplication.data.Video
+import by.androidacademy.firstapplication.db.MovieDao
+import by.androidacademy.firstapplication.db.VideoDao
 
 
 class MoviesRepository(
     private val tmdbServiceApi: TmdbServiceApi,
-    private val tmdbServiceMapper: TmdbServiceMapper
+    private val tmdbServiceMapper: TmdbServiceMapper,
+    private val movieDao: MovieDao,
+    private val videoDao: VideoDao
 ) {
 
     suspend fun getPopularMovies(): List<Movie> {
         val popularMoviesDto = tmdbServiceApi.getPopularMovies()
-        return tmdbServiceMapper.map(popularMoviesDto)
+        val movies = tmdbServiceMapper.map(popularMoviesDto)
+
+        movieDao.deleteAll()
+        movieDao.insertAll(movies)
+
+        return movies
     }
 
     suspend fun getMovieTrailerUrl(movie: Movie): String {
         val movieVideosDto = tmdbServiceApi.getMovieVideos(movie.id)
-        return tmdbServiceMapper.mapTrailerUrl(movieVideosDto.results.first())
+        val url = tmdbServiceMapper.mapTrailerUrl(movieVideosDto.results.first())
+
+        videoDao.insert(Video(movie.id, url))
+
+        return url
+    }
+
+    fun getCachedPopularMovies(): List<Movie> {
+        return movieDao.getAll()
+    }
+
+    fun getCachedMovieTrailerUrl(movie: Movie): String? {
+        val video = videoDao.getVideoByMovieId(movie.id)
+        return video?.url
+    }
+
+    fun deleteCachedData() {
+        movieDao.deleteAll()
+        videoDao.deleteAll()
     }
 }
